83065d257b0c82619fb80e7331867270c7990187
Iqbal Fauzi <iqbalfawz@gmail.com>
2023-04-07 15:06:01 +0700

    Call event user authorized on user login

--- modules/lib-user-auth-oauth2/controller/AuthController.php
+++ modules/lib-user-auth-oauth2/controller/AuthController.php
@@ -7,33 +7,37 @@
 
 namespace LibUserAuthOauth2\Controller;
 
+use LibUserAuthOauth2\Library\Storage;
 use LibUserAuthOauth2\Library\Authorizer;
 use LibEvent\Library\Event;
 
 class AuthController extends \Api\Controller
 {
 
-    private function errorView($params){
+    private function errorView($params)
+    {
         $this->res->render('user/auth/oauth2/error', $params);
         $this->res->send();
     }
 
-    public function authorizeAction() {
+    public function authorizeAction()
+    {
         $req = \OAuth2\Request::createFromGlobals();
         $res = new \OAuth2\Response();
 
         $server = Authorizer::getProvider()->getServer();
 
-        if(!$server->validateAuthorizeRequest($req, $res)){
-            if($res->getStatusCode() != 302)
+        if (!$server->validateAuthorizeRequest($req, $res)) {
+            if ($res->getStatusCode() != 302) {
                 return $this->errorView($res->getParameters());
+            }
             return $res->send();
         }
 
         $storage = Authorizer::getProvider()->getStorage();
         $app = $storage->getLastClient()['_app'];
 
-        if(!$this->user->isLogin()){
+        if (!$this->user->isLogin()) {
             $router = $this->config->libUserAuthOauth2->loginRoute;
             $next = $this->router->to($router, [], [
                 'next'      => $this->req->url,
@@ -45,28 +49,30 @@ class AuthController extends \Api\Controller
             return $this->res->redirect($next);
         }
 
-        if($this->req->getQuery('deny') == 1){
+        if ($this->req->getQuery('deny') == 1) {
             $server->handleAuthorizeRequest($req, $res, false);
             return $res->send();
         }
 
-        if($this->req->getPost('allow') == 1){
+        if ($this->req->getPost('allow') == 1) {
             $server->handleAuthorizeRequest($req, $res, true, $this->user->id);
             
-            if(module_exists('lib-event'))
+            if (module_exists('lib-event')) {
                 Event::trigger('user:authorized', $this->user->id);
+            }
 
             return $res->send();
         }
 
         $scopes = $this->req->getQuery('scope');
-        if($scopes){
+        if ($scopes) {
             $all_scopes = $storage->getAllScopes();
             $used_scopes = [];
             $scopes = explode(' ', $scopes);
-            foreach($scopes as $scope){
-                if(isset($all_scopes[$scope]))
+            foreach ($scopes as $scope) {
+                if (isset($all_scopes[$scope])) {
                     $used_scopes[] = $all_scopes[$scope];
+                }
             }
             $scopes = $used_scopes;
         }
@@ -80,7 +86,8 @@ class AuthController extends \Api\Controller
         return $this->res->send();
     }
 
-    public function revokeAction(){
+    public function revokeAction()
+    {
         $res = Authorizer::getProvider()
             ->getServer()
             ->handleRevokeRequest(
@@ -96,12 +103,22 @@ class AuthController extends \Api\Controller
         $res->send();
     }
 
-    public function tokenAction() {
+    public function tokenAction()
+    {
         $res = Authorizer::getProvider()
             ->getServer()
             ->handleTokenRequest(
                 \OAuth2\Request::createFromGlobals()
             );
+
+        if ($res->getStatusCode() == 200) {
+            $storage = Authorizer::getProvider()->getStorage();
+            $session = $storage->getUserDetails('session');
+
+            if ($session && isset($session['user_id'])) {
+                Event::trigger('user:authorized', $session['user_id']);
+            }
+        }
 
         $res->addHttpHeaders([
             'Access-Control-Allow-Origin'  => '*',
